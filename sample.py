7c0aaf9820758ceb4984e55995f4cc46d3dd05a8
maruti bandgar <marutibandgar12@gmail.com>
2019-05-12 09:03:18 -0400

    Return & Yield Statement USes

---
+++ sample.py
@@ -0,0 +1,8 @@
+#Difference Between Yield & Return 
+
+#Yield Statement can return  Generator Object
+#Return Statement can Return Actual Value
+def myFun(a):	#Function Defination
+	yield(a)
+
+print(myFun(5))	#Function Calling
