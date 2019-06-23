c5cef71f9dc9db39b880e86e26dc14621295c282
Maruti Bandgar <47293485+marutibandgar@users.noreply.github.com>
2019-06-23 05:55:07 -0400

    Create ADDtoNum.py

---
+++ ADDtoNum.py
@@ -0,0 +1,10 @@
+#Addition of two Number in Python. Number Accept from User
+
+num1 = int(input("Enter the First Number:"))
+
+num2 = int(input("Enter the Second Number:"))
+
+sum = num1 + num2
+
+print(sum)
+
