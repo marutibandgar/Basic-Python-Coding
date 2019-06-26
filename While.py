061ed8ffeaec8decdbc82cd48e66d9478b5a42b5
Maruti Bandgar <47293485+marutibandgar@users.noreply.github.com>
2019-06-27 00:09:40 +0530

    Add some Point

--- While.py
+++ While.py
@@ -3,9 +3,15 @@
 # Syntax:
 # while expression:
 #   statement(s)
+
+
+# The while loop in Python is used to iterate over a block of code as long as the test expression (condition) is true.
+# We generally use this loop when we don't know beforehand, the number of times to iterate.
+# While loops, like the ForLoop, are used for repeating sections of code - but unlike a for loop, the while loop will not run n times, but until a defined condition is no longer met. If the condition is initially false, the loop body will not be executed at all.
+
 # prints Hello G 3 Times 
 count = 0
 while (count < 3):     
     count = count+1
-    print("Hello Geek") 
+    print("Hello") 
  
