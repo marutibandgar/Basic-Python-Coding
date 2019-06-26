297731be294f72b3d74479e32a35435f243bef40
Maruti Bandgar <47293485+marutibandgar@users.noreply.github.com>
2019-06-26 23:56:52 +0530

    Loop Concept in Python

---
+++ While.py
@@ -0,0 +1,11 @@
+# Python programming language provides following types of loops to handle looping requirements.
+# While Loop
+# Syntax:
+# while expression:
+#   statement(s)
+# prints Hello G 3 Times 
+count = 0
+while (count < 3):     
+    count = count+1
+    print("Hello Geek") 
+ 
